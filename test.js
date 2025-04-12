fcb5f774e5a7664fff3c6df1280058091b169f83
Modulus19 <youngmodulus19@gmail.com>
2025-04-12 11:45:01 +0100

    Initial commit with solutions to 30 JavaScript exercises

---
+++ test.js
@@ -0,0 +1,24 @@
+//TESTING!!!!!!!!
+var firstName = "Emmanuel";
+let userName = "Modulus";
+const age = 21;
+
+console.log("My Names Are" + firstName);
+console.log(userName);
+console.log(age);
+
+let myFamilyNames = ["Emmanuel", "Angel", "Bulus", "Goodness"];
+myFamilyNames.push("Iykechukwu");
+myFamilyNames.pop();
+myFamilyNames.shift();
+myFamilyNames.unshift("Emmanuel");
+console.log(myFamilyNames[2]);
+
+let myId = {
+  firstName: "Emmanuel",
+  Lastname: "Tanko",
+  isMarried: false,
+  age: 21,
+};
+myId.userName = "Modulus";
+console.log(myId);
